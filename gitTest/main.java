222ad95518978c15cfc017b2c9def598d95c0ee3
unknown <obijuan2@frontier.com>
2019-08-23 16:30:51 -0700

    added hello again

--- gitTest/main.java
+++ gitTest/main.java
@@ -4,7 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hello world");
+System.out.println("hello world");
+
+System.out.print("hello again");
 	}
 
 }
